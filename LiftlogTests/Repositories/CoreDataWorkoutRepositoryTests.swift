217bac7e37fe94663fe3322c6ec3f880e586028a
Pavel Martynenkov <pavel@esys.de>
2026-03-31 15:02:04 +0200

    ExerciseSet and WorkoutRepository tests

---
+++ LiftlogTests/Repositories/CoreDataWorkoutRepositoryTests.swift
@@ -0,0 +1,16 @@
+//
+//  CoreDataWorkoutRepositoryTests.swift
+//  LiftlogTests
+//
+//  Created by Pavel Martynenkov on 31.03.26.
+//
+
+import Testing
+
+struct CoreDataWorkoutRepositoryTests {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
