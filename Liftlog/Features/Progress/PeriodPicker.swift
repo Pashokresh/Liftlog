f0974f46e3d4b2c7a6de364f9fe422accffee76c
Pavel Martynenkov <pashok.resh@gmail.com>
2026-05-06 15:46:27 +0200

    ExerciseProgressView (Part 1, Period Picker)

---
+++ Liftlog/Features/Progress/PeriodPicker.swift
@@ -0,0 +1,18 @@
+//
+//  PeriodPicker.swift
+//  Liftlog
+//
+//  Created by Pavel Martynenkov on 06.05.26.
+//
+
+import SwiftUI
+
+struct PeriodPicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PeriodPicker()
+}
